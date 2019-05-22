2109168334e5f7909003d636d90cdad6e825e956
Ricardo <geekricardozzz@gmail.com>
2019-05-22 08:42:41 +0800

    截图的类

--- capture.py
+++ capture.py
@@ -2,9 +2,7 @@ import tkinter
 import tkinter.filedialog
 import os, pdb
 import pyscreenshot as  ImageGrab
-from aip import AipOcr
 from PIL import ImageEnhance
-client = AipOcr('10684055', 'Y15dcjjkq2dLHB1NmdCN9ODI', 'keOYEXKG1RXnLyGLa4wfCr003kKE1zhh')
 
 from time import sleep
 root = tkinter.Tk()
@@ -13,7 +11,7 @@ root.geometry('100x50+400+300')
 #不允许改变窗口大小
 root.resizable(False,False)
 
-class MyCapture:
+class Capture:
     def __init__(self, png):
         #变量X和Y用来记录鼠标左键按下的位置
         self.X = tkinter.IntVar(value=0)
@@ -91,7 +89,9 @@ class MyCapture:
             #考虑鼠标左键从右下方按下而从左上方抬起的截图
             left, right = sorted([self.X.get(), event.x])
             top, bottom = sorted([self.Y.get(), event.y])
-            pic =ImageGrab.grab((left+1, top+1, right, bottom))
+            self.pic =ImageGrab.grab((left+1, top+1, right, bottom))
+            self.pw = right - left 
+            self.ph = bottom - top
             #关闭顶级容器
             self.top.destroy()
             #弹出保存截图对话框
@@ -102,11 +102,9 @@ class MyCapture:
                 # self.top.destroy()
         self.canvas.bind('<ButtonRelease-1>', onLeftButtonUp)
         self.canvas.pack(fill=tkinter.BOTH, expand=tkinter.YES)
-    def getText(self):
-        with open('./temp.bmp', 'rb') as img:
-            img = img.read()
-            result = client.basicGeneral(img)
-            return result
+
+
+
 #开始截图
 def buttonCaptureClick():
     #最小化主窗口
@@ -118,7 +116,7 @@ def buttonCaptureClick():
     im.save(filename)
     im.close()
     #显示全屏幕截图
-    w =MyCapture(filename)
+    w =Capture(filename)
     buttonCapture.wait_window(w.top)
     # pdb.set_trace()
     result = w.getText()
@@ -127,14 +125,6 @@ def buttonCaptureClick():
     root.state('normal')
     os.remove(filename)
 
-def printresult(json):
-    print('\n' + '-'*20)
-    if(json['words_result_num'] > 0):
-        for i in range(0,json['words_result_num'] ):
-            print(json['words_result'][i]['words'])
-    else:
-        print('+'* 5 + '  请重试！  ' + '+'*5)
-    print('\n' + '-'*20)
 
 buttonCapture = tkinter.Button(root, text='截图', command=buttonCaptureClick)
 buttonCapture.place(x=10, y=10, width=80, height=30)
